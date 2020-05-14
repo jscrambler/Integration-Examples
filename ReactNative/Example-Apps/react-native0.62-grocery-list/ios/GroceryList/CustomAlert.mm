a02749eaa06602e230cae5a9aa75355feb9bfe8e
Joao Pinto <joao.pinto@jscrambler.com>
2020-05-14 17:57:18 +0100

    fix(react-native-ios): update deprecated alert class

--- ReactNative/Example-Apps/react-native0.62-grocery-list/ios/GroceryList/CustomAlert.mm
+++ ReactNative/Example-Apps/react-native0.62-grocery-list/ios/GroceryList/CustomAlert.mm
@@ -10,16 +10,28 @@ RCT_EXPORT_MODULE(CustomAlert);
 
 - (dispatch_queue_t)methodQueue
 {
-    return dispatch_get_main_queue();
+  return dispatch_get_main_queue();
 }
 
 RCT_EXPORT_METHOD(showMessage:(NSString *)messageFromJs)
 {
-    NSString *cppString = [NSString stringWithUTF8String:greet().c_str()];
-  
-    NSString *myMessage = [NSString stringWithFormat:@"%@\nThis is from Objective-C\n%@", messageFromJs, cppString];
-    UIAlertView *alertView = [[UIAlertView alloc]initWithTitle:@"Native Code" message:myMessage delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"Ok", nil];
-    [alertView show];
+  NSString *cppString = [NSString stringWithUTF8String:greet().c_str()];
+
+  NSString *myMessage = [NSString stringWithFormat:@"%@\nThis is from Objective-C\n%@", messageFromJs, cppString];
+
+  UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Native Code" message:myMessage preferredStyle:UIAlertControllerStyleAlert];
+
+  UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {}];
+
+  [alert addAction:defaultAction];
+
+  UIViewController *topController = [UIApplication sharedApplication].keyWindow.rootViewController;
+
+  while (topController.presentedViewController) {
+    topController = topController.presentedViewController;
+  }
+
+  [topController presentViewController:alert animated:YES completion:nil];
 }
 
 @end
